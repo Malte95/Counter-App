2d702bef92992cc676d6a633633bf761141c15d0
Malte95 <107354881+Malte95@users.noreply.github.com>
2023-05-05 17:18:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+let saveEL = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+ let count = 0
+
+function increment() {
+     count += 1
+     countEl.innerText = count
+ }
+
+ function save() {
+    let countStr = count + " - "
+    saveEL.textContent += countStr 
+    countEl.textContent = 0
+    count = 0
+}
+
+
+
+
+    
+
+
+
+
+
+ 
+
+
+
+
+
+
+
